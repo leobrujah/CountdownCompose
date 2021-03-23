214b70dbf1e24188e478f641fbf19df38c576723
Leo Santos <lsantos@hubspot.com>
2021-03-23 16:44:45 +0000

    compose view only updating after second click

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,22 +15,41 @@
  */
 package com.example.androiddevchallenge
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.AbstractComposeView
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyTheme {
-                MyApp()
+       setContentView(R.layout.main)
+        findViewById<ButtonPrimary>(R.id.myButton).apply {
+            text = "My Compose in XML"
+            onClickListener = {
+                Toast.makeText(this@MainActivity, "Worked", Toast.LENGTH_LONG).show()
+                isEnabled = false
             }
         }
     }
@@ -57,5 +76,56 @@ fun LightPreview() {
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
         MyApp()
+    }
+}
+
+@Composable
+fun ButtonPrimary(
+    text: String,
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) = CanvasButton(
+    enabled = enabled,
+    onClick = onClick
+) {
+    Text(text = text)
+}
+
+@Composable
+internal fun CanvasButton(
+    modifier: Modifier = Modifier,
+    border: BorderStroke? = null,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) = Button(
+    modifier = modifier,
+    border = rememberUpdatedState(if (enabled) border else null).value,
+    enabled = enabled,
+    onClick = onClick
+) {
+    content()
+}
+
+class ButtonPrimary @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : AbstractComposeView(context, attrs, defStyle) {
+
+    var text by mutableStateOf("")
+    var onClickListener by mutableStateOf({})
+    private var _enabled by mutableStateOf<Boolean>(true)
+
+    @Composable
+    override fun Content() {
+        MaterialTheme {
+            ButtonPrimary(text = text, enabled = _enabled, onClick = onClickListener)
+        }
+    }
+
+    override fun isEnabled(): Boolean {
+        _enabled = super.isEnabled()
+        return _enabled
     }
 }
